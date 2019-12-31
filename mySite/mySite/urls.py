60c36c439058c6e444d1f391ff180c9e6daff618
Arnie09 <arnabchanda964@gmail.com>
2019-12-31 22:04:29 +0530

    Set up visual elements of blog

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls'), name = 'blog'),
 ]
